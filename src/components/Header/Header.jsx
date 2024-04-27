8862978a9680f0b504edd5e6e4f9a716a9910aca
Pavlína Krátká <pajus.kratka@gmail.com>
2024-04-27 16:53:15 +0200

    oživení detailu objednávky s komponentami a variantou záhlaví

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import './Header.css';
 
-export const Header = () => {
- return ( 
-     <header>
+export const Header = ({ showMenu = true }) => {
+  return showMenu ? (
+    <header>
       <div className="header__content container">
         <div className="site-logo"></div>
 
@@ -15,7 +15,17 @@ export const Header = () => {
             <a href="#contact">kontakt</a>
           </nav>
         </div>
-
       </div>
     </header>
-)}
+  ) : (
+    <header>
+      <div className="header__content container">
+        <div className="site-logo"></div>
+
+        <nav className="inline-nav">
+          <a href="/">Hlavní stránka</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
