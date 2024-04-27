e89b1b50a264a1dcba0b5e81157c34d9ddf9b56c
Pavlína Krátká <pajus.kratka@gmail.com>
2024-04-27 11:09:59 +0200

    vytvoření komponent a zprovoznění odkazů

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+export const Banner = () => {
+return (
+<section id="home" className="banner">
+        <div className="container">
+          <div className="banner__content">
+            <h1>Přijď to ochutnat!</h1>
+            <p>Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.</p>
+          </div>
+        </div>
+      </section>
+)}
