43931d3318d30b88451906b549307cd67757fdcd
Pavlína Krátká <pajus.kratka@gmail.com>
2024-04-27 16:30:15 +0200

    zprovoznění tlačítka pro objednání nápoje

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -1,28 +1,29 @@
 import './Drink.css';
 import { Layer } from '../Layer/layer';
 
-export const Drink = ({ id, name, ordered, image, layers }) =>  {
-
-  const layerElm = layers.map(({label, color }) => (
+export const Drink = ({ id, name, ordered, image, layers }) => {
+  const layerElm = layers.map(({ label, color }) => (
     <Layer key={label} color={color} label={label} />
-  ))
+  ));
   return (
-  <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src={image} />
-                </div>
-                <div className="drink__info">
-                  <h3>{name}</h3>
-                  {layerElm}
-                </div>
-              </div>
-              <form className="drink__controls">
-                <input type="hidden" className="order-id" value="1" />
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </form>
-            </div>
- )
-}
+    <div className="drink">
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+          {layerElm}
+        </div>
+      </div>
+      <form data-id={id} className="drink__controls">
+        <input type="hidden" className="order-id" value="1" />
+        <button
+          className={ordered ? 'order-btn .order-btn--ordered' : 'order-btn'}
+        >
+          {ordered ? 'Zrušit' : 'Objednat'}
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,32 +8,54 @@ import { Footer } from '../components/Footer/Footer';
 import '../global.css';
 import './index.css';
 
-const response = await fetch("http://localhost:4000/api/drinks")
-const json = await response.json()
-const drinks = json.data
- console.log(drinks)
-
+const response = await fetch('http://localhost:4000/api/drinks');
+const json = await response.json();
+const drinks = json.data;
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
     <main>
       <Banner />
-      <Menu drinks={drinks}/>
+      <Menu drinks={drinks} />
       <Gallery />
-      <Contact />      
+      <Contact />
     </main>
     <Footer />
-  </div>
+  </div>,
 );
 
-document.querySelector(".nav-btn").addEventListener("click", () => {
-    document.querySelector(".rollout-nav").classList.toggle("nav-closed")
-})
+document.querySelector('.nav-btn').addEventListener('click', () => {
+  document.querySelector('.rollout-nav').classList.toggle('nav-closed');
+});
 
-const navElm = document.querySelector(".rollout-nav");
-navElm.addEventListener("click", () => {
-  navElm.classList.add("nav-closed")
-})
+const navElm = document.querySelector('.rollout-nav');
+navElm.addEventListener('click', () => {
+  navElm.classList.add('nav-closed');
+});
 
+const formElm = document.querySelectorAll('form');
+formElm.forEach((form) => {
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    console.log(form.dataset.id);
 
+    console.log(drinks);
+
+    const foundDrink = drinks.find((drink) => drink.id === Number(form.dataset.id));
+    const ordered = foundDrink.ordered;
+    console.log(foundDrink);
+
+    await fetch(`http://localhost:4000/api/drinks/${form.dataset.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([
+        { op: 'replace', path: '/ordered', value: !ordered },
+      ]),
+    });
+
+    window.location.reload();
+  });
+});
